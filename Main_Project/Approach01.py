ce4ed5904cd86d133deb06caddf636fbda314e63
Jahid H Emon <je2264r@gre.ac.uk>
2025-02-27 15:53:25 +0000

    Updated extraction of decimal

--- Main_Project/Approach01.py
+++ Main_Project/Approach01.py
@@ -22,27 +22,38 @@
 
 
 class Extracts_prime_from_binary:
-    def __init__(self, binarynumber):
-        self.binarynumber = binarynumber
-        self.possible_binary_substrings = ()
-        self.decimal_values = []
-        self.list_of_primes = []
+    def __init__(self, binarynumber):                 #initializing the method
+        self.binarynumber = str(binarynumber)         #takes input as strings
+        self.possible_binary_substrings = set()     #stores all the possible binary substrings
+        self.decimal_values = set()
+        self.list_of_primes = set()
         
-    def extracts_enique_binary_num (self):
-        given_binary_string = str(self)
-        substrings = set()
+    def extracts_enique_binary_num (self):            #method that extracts unique substrings from the string
         
-        for i in range (len(given_binary_string)):
-            for j in range (i + 1, (len(given_binary_string)) +1 ):
-                substrings.add(given_binary_string [i : j])
+        for i in range (len(self.binarynumber )):     #cheks the len of the strings with starting index "i"
+            for j in range (i + 1, (len(self.binarynumber )) +1 ):    #checks the ending with j = from i+1 to len +1
+                self.possible_binary_substrings.add(self.binarynumber  [i : j])  #extracts the substrings
                 
-        return sorted(substrings)
+        return sorted(self.possible_binary_substrings)          #returns sorted binary substrings
+    
+    
+    
+    def make_decimal (self):                             #method for extracting decimal vlaues
+        self.decimal_values = set ()                      #initializing decimal_values as a set
+        for binary_strings in self.possible_binary_substrings:      #loopint through the binary SStrings 
+            decimal_value = int(binary_strings , 2)          #using built in func to get decimal vlaues
+            self.decimal_values.add(decimal_value)           #adding the values to the decimal_vlaues list
+        
+        return sorted(self.decimal_values)
+    
     
     
 num = 10101
-result = Extracts_prime_from_binary.extracts_enique_binary_num(num)
+classifier = Extracts_prime_from_binary(num)
+result = classifier.extracts_enique_binary_num()  # Extracts unique binary substrings
+for_decimal_values = classifier.make_decimal()
+print("\n")   
 print(result)
-    
-    
+print(for_decimal_values)    
             
 
